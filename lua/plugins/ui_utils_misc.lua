18dd4860966212cf752c69f53604ac3cfcf14662
David Bellot <david.bellot@gmail.com>
2023-09-27 15:16:44 +1000

    improved lua line appearance

--- lua/plugins/ui_utils_misc.lua
+++ lua/plugins/ui_utils_misc.lua
@@ -42,7 +42,28 @@ return {
 {'lukas-reineke/indent-blankline.nvim', -- indent lines
     opts = { show_current_context = true,show_current_context_start = true}},
 
-{'nvim-lualine/lualine.nvim', opts = { options = { theme = 'ayu_dark' }}},
+{'nvim-lualine/lualine.nvim', opts = {
+    options = {
+        theme = 'ayu_dark',
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+        sections = {
+            lualine_a = {{'mode', separator = { left = '' }, right_padding = 2 },},
+            lualine_b = {'filename', 'branch'},
+            lualine_c = {'fileformat'},
+            lualine_x = {},
+            lualine_y = {'filetype', 'progress'},
+            lualine_z = {{'location', separator = { right = '' }, left_padding = 2 },},},
+        inactive_sections = {
+            lualine_a = { 'filename' },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = { 'location' },},
+        tabline = {},
+        extensions = {},
+}}},
 
 {'lewis6991/gitsigns.nvim', -- Git signs in the vi gutter (+ on status line)
     event = "VeryLazy",
