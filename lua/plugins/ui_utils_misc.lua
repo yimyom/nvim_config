3e0a01c94231d490d74a8276f5d49c358f4b90fd
David Bellot <david.bellot@gmail.com>
2023-10-09 16:04:52 +1100

    fixed indentblankline config

--- lua/plugins/ui_utils_misc.lua
+++ lua/plugins/ui_utils_misc.lua
@@ -42,8 +42,6 @@ return {
 {'lukas-reineke/indent-blankline.nvim', main='ibl', -- indent lines
     config = function()
         require('ibl').setup({
-            -- show_current_context = true,
-            show_current_context_start = true,
             scope = { enabled = true },
         })
         end
