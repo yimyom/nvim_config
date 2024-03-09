66994881fc69255fcc368d91901e4216e933594c
David Bellot <david.bellot@gmail.com>
2024-03-09 14:19:01 +1100

    small fix

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -102,6 +102,8 @@ return {
                             library = vim.api.nvim_get_runtime_file('', true),
                             checkThirdParty = false,
                         },
+                    },
+                },
             },
             pyright = {},
 --          pylsp = {
