cb5831bd40aea32801d1bab01a51d460c6d23bdb
David Bellot <david.bellot@gmail.com>
2024-04-18 17:01:59 +1000

    changed gitsigns to ascii

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -51,9 +51,9 @@ return {
     event = {'BufReadPre','BufNewFile'},
     opts = {
          signs = {
-            add = { text = " " }, 
-            change = { text = " " }, 
-            delete = { text = " " }, 
+            add = { text = "+" },
+            change = { text = "~" },
+            delete = { text = "-" },
             topdelete = { text = "‾" },
             changedelete = { text= "~" },
             untracked = { text = "┆" },
