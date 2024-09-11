ec6d4581fea83e090abf56be40353230210e67df
David Bellot <david.bellot@gmail.com>
2024-09-11 10:45:19 +1000

    improved file manager ui

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -31,29 +31,35 @@ return {
     },
 },
 
---{'nvim-tree/nvim-tree.lua', -- file manager
---    opts = {
---        sort_by = 'case_sensitive',
---        on_attach = on_attach_nvim_tree,
---        view = { number = true }
---    },
---    keys = {
---        { '<leader>f', '<cmd>NvimTreeToggle<cr>', mode='n', noremap=true, silent=true, desc='File manager' }
---    },
---},
-
 {'nvim-neo-tree/neo-tree.nvim',
     lazy = true,
     cmd = 'Neotree',
-    branch = 'v3.x',
+--    branch = 'v3.x',
     dependencies =
     {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
         'MunifTanjim/nui.nvim',
     },
+    opts =
+    {
+        close_if_last_window = true,
+        popup_border_style = 'rounded',
+        enable_git_status = true,
+        enable_diagnostics = true,
+        open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+        use_libuv_file_watcher = true,
+
+        event_handlers =
+        {
+            event = "neo_tree_buffer_enter",
+            handler = function()
+                vim.opt_local.number = true
+            end,
+        },
+    },
     keys = {
-        { '<leader>f', '<cmd>Neotree<cr>', mode='n', noremap=true, silent=true, desc='File manager' }
+        { '<leader>f', '<cmd>Neotree toggle<cr>', mode='n', noremap=true, silent=true, desc='File manager' }
     },
 },
 
