51c75ea67c054ec1532b395686abc260fbb237bb
David Bellot <david.bellot@gmail.com>
2025-09-28 17:56:01 +1000

    disable all llms for the time being

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -9,54 +9,54 @@
 
 return {
 
-{'olimorris/codecompanion.nvim',
-    lazy = false,
-    opts =
-    {
-        strategies =
-        {
-            chat   = { adapter = 'deepseek' },
-            inline = { adapter = 'deepseek' },
-            cmd    = { adapter = 'deepseek' },
-        },
-        adapters =
-        {
-           anthropic = function()
-                return require("codecompanion.adapters").extend("anthropic",
-                    { env = { api_key = os.getenv('ANTHROPIC_API_KEY'), }, })
-                end,
-            deepseek = function()
-                return require('codecompanion.adapters').extend('deepseek',
-                    {env = { api_key = os.getenv("DEEPSEEK_API_KEY") }, })
-                end,
---            gemini = function()
---                return require('codecompanion.adapters').extend('gemini',
---                    {env = { api_key = 'GEMINI_API_KEY' }, })
+--{'olimorris/codecompanion.nvim',
+--    lazy = false,
+--    opts =
+--    {
+--        strategies =
+--        {
+--            chat   = { adapter = 'deepseek' },
+--            inline = { adapter = 'deepseek' },
+--            cmd    = { adapter = 'deepseek' },
+--        },
+--        adapters =
+--        {
+--           anthropic = function()
+--                return require("codecompanion.adapters").extend("anthropic",
+--                    { env = { api_key = os.getenv('ANTHROPIC_API_KEY'), }, })
 --                end,
---            mistral = function()
---                return require('codecompanion.adapters').extend('mistral',
---                    {env = { api_key = 'MISTRAL_API_KEY' }, })
+--            deepseek = function()
+--                return require('codecompanion.adapters').extend('deepseek',
+--                    {env = { api_key = os.getenv("DEEPSEEK_API_KEY") }, })
 --                end,
---            ollama = function()
---                return require('codecompanion.adapters').extend('ollama',
---                    { schema = { model = { default = 'llama3.1:latest', },
---                                 num_ctx = { default = 20000,}, }, })
---                end,
---            openai = function()
---                return require('codecompanion.adapters').extend('openai',
---                    {
---                        opts = { stream = true, },
---                        env = { api_key = 'OPENAI_API_KEY', },
---                        schema = { model = { default = function() return 'gpt-4.1' end, }, },
---                    })
---                end,
---            xai = function()
---                return require('codecompanion.adapters').extend('xai',
---                    {env = { api_key = 'XAI_API_KEY' }, })
---                end,
-        },
-    },
-},
+----            gemini = function()
+----                return require('codecompanion.adapters').extend('gemini',
+----                    {env = { api_key = 'GEMINI_API_KEY' }, })
+----                end,
+----            mistral = function()
+----                return require('codecompanion.adapters').extend('mistral',
+----                    {env = { api_key = 'MISTRAL_API_KEY' }, })
+----                end,
+----            ollama = function()
+----                return require('codecompanion.adapters').extend('ollama',
+----                    { schema = { model = { default = 'llama3.1:latest', },
+----                                 num_ctx = { default = 20000,}, }, })
+----                end,
+----            openai = function()
+----                return require('codecompanion.adapters').extend('openai',
+----                    {
+----                        opts = { stream = true, },
+----                        env = { api_key = 'OPENAI_API_KEY', },
+----                        schema = { model = { default = function() return 'gpt-4.1' end, }, },
+----                    })
+----                end,
+----            xai = function()
+----                return require('codecompanion.adapters').extend('xai',
+----                    {env = { api_key = 'XAI_API_KEY' }, })
+----                end,
+--        },
+--    },
+--},
 
 --{'yetone/avante.nvim',
 --    event = 'VeryLazy',
