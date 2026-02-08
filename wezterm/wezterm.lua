3f2ca37cd5b5bd9da5e9db668912b44c7dbff1ac
David Bellot <david.bellot@gmail.com>
2026-02-09 08:47:55 +1100

    removed ligatures in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,6 @@ config.window_decorations = 'RESIZE'
 
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
-
 config.window_padding =
 {
     left = '0.25cell',
@@ -16,10 +15,11 @@ config.window_padding =
     top = '0.25cell',
     --bottom = '0.5cell',
     bottom = '1cell',
-
 }
 
 config.adjust_window_size_when_changing_font_size = false
 config.use_resize_increments = true
+
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 return config
